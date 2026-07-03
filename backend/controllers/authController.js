9fcc4a02d1189fecd24f827f59a6ec1b1a95e246
Rishexx <privaterishabhh@gmail.com>
2026-07-03 17:34:59 +0530

    Fix Auth Glitch

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -19,7 +19,7 @@ const googleCallback = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+      sameSite: 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
